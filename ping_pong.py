89b24bf7f7c14c1368ba8950ea33b8897c7d7cda
ChocoBite <56428913+ChocoBite@users.noreply.github.com>
2021-05-22 13:02:06 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -5,9 +5,20 @@ pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Ping Pong")
 clock = pygame.time.Clock()
-window_pp = pygame.display.set_mode((400, 300))
-
+window = pygame.display.set_mode((400, 300))
+#background = transform.scale(image.load(''), (700, 500))
+#window.feel('blue')
 FPS = 60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, image_pl, x_pl, y_pl, speed_pl):
+        super().__init__()
+        self.image = transform.scale(image.load(image_pl), (80,80))
+        self.rect.x = x_pl
+        self.rect.y = y_pl
+        self.speed = speed_pl
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.reset.y))
 
 game = True
 while game:
